177d01e733c9d576e3faf3961167866ca749de4b
Razvan <razvan.mustata@gmail.com>
2023-11-05 11:50:20 +0200

    add exchange rate

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+// import { BNR } from 'bnr';
+import BNR = require("bnr")
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+   
+  getHello(): any {
+
+    BNR.convert(1, "EUR", "RON", function (err, amount, output) {
+      if (err) { return console.error(err); }
+      console.log(`Result: ${amount}`);
+      console.log(`${output.input.amount} ${output.input.currency} is ${output.output.amount} ${output.output.currency}`);
+      // Result: 107.72102819395911
+      // 100 EUR is 107.72102819395911 USD
+  });
   }
 }
