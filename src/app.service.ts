741e3376d1970d6b89813f4425fba7132bb213de
Razvan <razvan.mustata@gmail.com>
2023-11-05 11:56:41 +0200

    add exchange rate correction2

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@nestjs/common';
 // import { BNR } from 'bnr';
 import BNR = require("bnr")
+import { LessThan } from 'typeorm';
 
 @Injectable()
 export class AppService {
    
   getHello(): any {
 
-    BNR.convert(1, "EUR", "RON", function (err, amount, output) {
+    let result = BNR.convert(1, "EUR", "RON", function (err, amount, output) {
       if (err) { return console.error(err); }
       console.log(`Result: ${amount}`);
-      console.log(`${output.input.amount} ${output.input.currency} is ${output.output.amount} ${output.output.currency}`);
-      // Result: 107.72102819395911
-      // 100 EUR is 107.72102819395911 USD
+ //     console.log(`${output.input.amount} ${output.input.currency} is ${output.output.amount} ${output.output.currency}`);
   });
+  return result;
   }
 }
