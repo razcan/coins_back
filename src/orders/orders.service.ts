e7750a7f8a209e2cfb3a6dfbd90905404429522e
Razvan <razvan.mustata@gmail.com>
2023-11-07 15:07:58 +0200

    add REST for Order

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -12,29 +12,28 @@ import { InjectRepository } from '@nestjs/typeorm';
 export class OrdersService {
   constructor(
     @InjectRepository(Order)
-    private readonly createOrderRepository: Repository<Order>,
+    private readonly orderRepository: Repository<Order>,
     @InjectRepository(OrderDetails)
-    private readonly createorderdetailsRepository: Repository<OrderDetails>,
+    private readonly orderdetailsRepository: Repository<OrderDetails>,
   ) {}
 
   async create(createOrderDto: any) {
  
     const order_details = createOrderDto.DetailsOrder.length;
 
-    const order_rezult = await this.createOrderRepository.save(createOrderDto);
+    const order_rezult = await this.orderRepository.save(createOrderDto);
 
     for (let i=0 ; i<order_details; i++)
     {
       createOrderDto.DetailsOrder[i].orderId=order_rezult.id;
-     //console.log('xx',createOrderDto.DetailsOrder[i])
     }
     console.log(createOrderDto.DetailsOrder);
-    await this.createorderdetailsRepository.save(createOrderDto.DetailsOrder);
+    await this.orderdetailsRepository.save(createOrderDto.DetailsOrder);
    return 'This action adds a new order: '+createOrderDto;
   }
 
   findAll() {
-    return this.createOrderRepository.find(
+    return this.orderRepository.find(
       {
         relations: {
           orderdetails: true,
@@ -44,14 +43,64 @@ export class OrdersService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} order`;
+    return this.orderRepository.find(
+      {where: {
+        id: id},
+    relations: {
+      orderdetails: true,
+              },
+      }
+    );
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
+ async update(id: number, updateOrderDto: any) {
+
+    const tobe_updated = await this.orderRepository.find(
+      {where: {id: id},
+      // relations: { orderdetails: true,},
+      }
+    )
+    tobe_updated[0].OrderDate =updateOrderDto.OrderDate;
+    tobe_updated[0].Customer =updateOrderDto.Customer;
+    tobe_updated[0].TotalAmount =updateOrderDto.TotalAmount;
+    tobe_updated[0].OrderStatus =updateOrderDto.OrderStatus;
+    tobe_updated[0].ShippingAddress =updateOrderDto.ShippingAddress;
+    tobe_updated[0].PaymentMethod =updateOrderDto.PaymentMethod;
+    tobe_updated[0].PaymentMethod =updateOrderDto.PaymentMethod;
+
+    let details_existent = updateOrderDto.DetailsOrder;
+
+    const details_to_update = await this.orderdetailsRepository.find({
+      where: {
+        orderId: id
+      },
+  })
+
+  for (let i=0 ; i<details_to_update.length; i++)
+  {
+    let idc = details_to_update[i].id
+    details_to_update[i].CoinId=details_existent[i].CoinId;
+    details_to_update[i].Quantity=details_existent[i].Quantity;
+    details_to_update[i].UnitPrice=details_existent[i].UnitPrice;
+    details_to_update[i].Discount=details_existent[i].Discount;
+    details_to_update[i].Total=details_existent[i].Total;
+    this.orderdetailsRepository.update(idc, details_to_update[i])
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+   return this.orderRepository.update(id, tobe_updated[0]);
   }
-}
+
+  async remove(id: number) {
+
+    const details_to_remove = await this.orderdetailsRepository.find({
+      where: {
+        orderId: id
+      },
+  })
+  
+  for (let i=0 ; i<details_to_remove.length; i++){
+    this.orderdetailsRepository.delete({orderId: id});
+  }
+     this.orderRepository.delete({id});
+  }
+  }
